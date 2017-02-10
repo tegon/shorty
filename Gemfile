af592802d54d4c60ed3201adac75c3d971f2ea66
Leonardo Tegon <ltegon93@gmail.com>
2017-02-10 21:46:53 -0200

    Add `dotenv` gem to load environment variables

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'sinatra-contrib', '1.4.7', require: 'sinatra/reloader'
 gem 'oj', '2.18.1'
 gem 'sequel', '4.43.0'
 gem 'sequel_pg', '1.6.17', require: 'sequel'
+gem 'dotenv', '2.2.0'
 
 group :development do
   gem 'tux', '0.3.0'
--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,7 @@ require 'rubygems'
 require 'bundler'
 Bundler.require
 
+require 'dotenv/load'
+
 Dir.glob(File.expand_path('../initializers/**/*.rb', __FILE__)).each { |f| require f }
 Dir.glob(File.expand_path('../../app/**/*.rb', __FILE__)).each { |file| require file }
