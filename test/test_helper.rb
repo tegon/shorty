3c5a6b11954eff44c438ce8f178104dbf288c962
Leonardo Tegon <ltegon93@gmail.com>
2017-02-10 23:14:58 -0200

    Setup test suite Added database config for testing in `.env.test` file. Created `test_helper.rb` file to load the application enviroment and run `DatabaseCleaner` before each test.

---
+++ test/test_helper.rb
@@ -0,0 +1,27 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'bundler'
+
+Bundler.require(:default, :test)
+Dotenv.load('.env.test')
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rack/test'
+require 'minitest/autorun'
+
+def app
+  Shorty.new
+end
+
+class MiniTest::Test
+  include Rack::Test::Methods
+
+  DatabaseCleaner.clean_with :truncation
+  DatabaseCleaner.strategy = :truncation
+
+  def before_setup
+    super
+    DatabaseCleaner.clean
+    DatabaseCleaner.start
+  end
+end
