db3f3e7f3a08e1d25b7739ef5902e29c630e0520
Leonardo Tegon <ltegon93@gmail.com>
2017-02-11 09:16:58 -0200

    Remove the unique constraint for Click's `link_id` A `Link` will have many `Click`s, so the `link_id` can't be unique.

--- db/migrate/003_create_clicks.rb
+++ db/migrate/003_create_clicks.rb
@@ -4,7 +4,7 @@ Sequel.migration do
       uuid         :id, default: Sequel.function(:uuid_generate_v4), primary_key: true
       timestamptz  :created_at, default: Sequel.function(:now), null: false
       timestamptz  :updated_at, default: Sequel.function(:now), null: false
-      foreign_key  :link_id, :links, unique: true, type: 'uuid', null: false
+      foreign_key  :link_id, :links, type: 'uuid', null: false
     end
   end
 end
